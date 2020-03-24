e2053c517cb322c8769ac616854fc0818cd8b103
Aamir Soni <aamirsoni1551@gmail.com>
2020-03-24 16:56:47 +0000

    server-cient done

--- Client.java
+++ Client.java
@@ -1,5 +1,142 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+//READ THIS CODE BEFORE THE SERVER.JAVA CODE
+
 class Client {
+
+    //SERVER = A powerful computer, and has the resources and data a client may need, and usually sits their idle and waits for clients to connect
+    //Client = A computer that wants to connect to the server, in order to fulfil some task, and is the one that reaches out to the client to send data
+
+    //WHAT IS A PORT?
+    //To communicate wirelessly computers send radio waves to other computers.
+    //Once the recieving computer recieves the radio waves, it then needs to decipher this signal.
+    //Once deciphered, it can then determine which application this message was sent to, since at any one
+    //time many applications will be communicating with other computers around the world. E.g, the web browser (usually chrome), may
+    //be interacting with the BBC server, which will be based in the UK, but another application say minecraft may be interacting with
+    //a different server, based in a different location. Hence, once a computer recieves a signal, it needs to determine which application
+    //this signal is talking to. This is accomplished via ports. Each application sends data via a specific port, choosen by the OS.
+    //Every application however, knows which port (is preconfigured by the programmer), to send their data to on the external server, they
+    //are communicating with. So the web browser on a computer thats visiting the bbc website, knowns which port to send data to on the bbc server
+    //(A server is just a computer). However, the client (who's using the web browser and visiting the bbc website), can use whatever port they want to, 
+    //to send a request to the bbc server. NOTE: There is no such thing as port, its all just the operating systems doing, to help determine which electrical 
+    //signal recieved belongs to what application.
+
+    //Hence when you start making your own applications, that need to connect with other computers, you must specify the port that you are sending data to, on the
+    //other computer. Note, you do NOT need to say 
+
+    //WHAT IS A SOCKET
+    //A socket in general is basically an IP address plus by a port in the format IP_ADDRESS:PORT. So, if IP_ADDRESS = 134.54.12.3 and PORT = 1234, 
+    //then the socket address would be socket address = 134.54.12.3:1234. This allows every application to be uniquely specified. Since, an ip uniquely specifies
+    //the computer in the whole world, and the port, helps specify an application on that particular computer. Hence, by having the socket address, we may
+    //connect directly to that application on that computer and send it data and it can then recieve it. Hence, when ever communicating with a client
+    //it is a MUST to have a connection to that particular socket, so we can interact with an application on that computer.
+
     public static void main(String[] args) {
 
+        Socket clientSocket = null; //Defining this variable here, so I can use it outside the try/catch block
+        DataOutputStream writetoOtherComputer = null; //Defining this variable here, so I can use it outside the try/catch block
+        DataInputStream readFromOtherComputer = null;  //Defining this variable here, so I can use it outside the try/catch block
+
+        try {
+            //clientSocket is a connetion to a socket, and allows us to send data uniquely to an application on a computer in the world
+            //using that socket address. //localhost is the ipaddress, and 1234 is our port number. localhost basically means the ip address of this computer
+            //hence, when this comuter tries sending data to localhost (which is the ipaddress of the computer we are sending data to), it ends up
+            //sending the data to itself. To Send data to another computer, we must change localhost to that computers IP address
+            
+            clientSocket = new Socket("localhost", 1234); //NOTE: 1234 is not the port our client is using, but the port 
+            //we are going to connect to on the server(other computer we are communicating ith)
+
+            //Reason for using the try/catch statement, is because this connection may not be able to be made, since the IPaddress may not exist, or
+            //the port number may not exist. NOTE:PORT numbers 0-1024 are reserved and should NOT be used. They are reserved for special applications
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+        try {
+            
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+
+        try {
+            //Reason for putting this inside the try/catch block was because if the clientSocket didnt exist, then clientSocket.getOutputStream would not work
+            //since the clientSocket is still null, and doesnt exist. Same with client.getInputStream.
+            writetoOtherComputer = new DataOutputStream(new BufferedOutputStream(clientSocket.getOutputStream()));
+            readFromOtherComputer = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+
+        //Lets say hello to the other computer (In this case the other computer we are talking to is the server)
+        //The other computer is a server, since it will already be listening at that port and doesnt initiate a connection, instead we do, by creating 
+        //a connection to it at that IP address and port(IP address in this case is localhost meaning our own computer is also our server, but this usually
+        //isint the case, and we have an actual IP address instead of "localhost")
+        try {
+            //reason for wrapping this in a try/catch block, is that writeToOtherComputer may be null still if clientSocket didnt exist, (which is only possible
+            //if we provided incorrect ipaddresds or port number)
+            writetoOtherComputer.writeUTF("hello"); //WriteUTF writes a string to the other computer (UTF = unicaode transofrmation format = basically strings)
+            writetoOtherComputer.flush(); //Remember to always flush, after every send
+            writetoOtherComputer.writeInt(10); //writeInt writes an integer to other computer
+            writetoOtherComputer.flush(); //Remember to always flush, after every send
+            writetoOtherComputer.writeBoolean(false); //you get the jist...
+            writetoOtherComputer.flush(); //Remember to always flush, after every send
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+        //lets try reading data from the other computer
+        //I'll let you guess why its wrapped in try/catch block
+        try {
+            
+            String stringOutput = readFromOtherComputer.readUTF(); //reading in a string from the other computer
+            Boolean booleanOutput = readFromOtherComputer.readBoolean(); //reading in a boolean from the other computer
+
+            System.out.println(stringOutput);
+            System.out.println(booleanOutput);
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+        //Lets try sending our own data to the other computer, that we input into the console
+        try {
+
+            Scanner in = new Scanner(System.in);
+            String stringToSendToOtherComputer = in.nextLine();
+
+            writetoOtherComputer.writeUTF(stringToSendToOtherComputer);
+            writetoOtherComputer.flush();
+
+
+            in.close(); //close method of taking input from console, since it will not be used again
+
+            //lets read data from client now
+            String inputFromOtherComputer = readFromOtherComputer.readUTF();
+            System.out.println(inputFromOtherComputer);
+
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+
+        //Finally close all streams and sockets. (Closing socket = closing connection, closing streams = closing method of reading and writing to clients)
+        try {
+
+            //First close streams
+            writetoOtherComputer.close();
+            readFromOtherComputer.close();
+
+            //The close socket
+            clientSocket.close();
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
     }
 }
+
+//ofcourse, i could have wrapped all this in onetr/catch block, but for sake of learning i did not
--- Server.java
+++ Server.java
@@ -1,27 +1,84 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.ServerSocket;
+import java.net.Socket;
+
+//READ THE CLIENT.JAVA CODE BEFORE THIS CODE
 
 class Server {
     public static void main(String[] args) {
     
+
+        //This computer is our Server. It does not start a connection with any computer. Instead is listens at a predefined port on, and whenever it 
+        //recieves a connection from another computer at the port it is listening on, it starts talking to it.
         
         try {
 
-            //so a port is basically like a electrical socket, 
-            //so imagine a computer having loads of electrical sockets, except that you cant see them, makes sense? yes like a connection
-            //well yea, and every new client, needs a port to connect to. Soy these electrical sockets are our ports
-            //btw i said a port is like an electrical socket.
-            //i did not say a computer socket is like an electrical socket
-            //is that clear?, cos port != computer socket, yea? lool yh
-            //well a computer socket is basically ip address and socket joined together as a string
-            //so if ur ip address was 123.1.9.3 and port was 1234
-            //then ur socket address would be 123.1.9.3::1234   -- get it? yh
-            //so anyways, when a client makes a conneciton with a server, they need to connect to a port on the server, 
-            //clients are preconfigured with the port they need to connect on the computer
+            //A serverSocket defines the socket address of our server, and hence it knows on which port to listen on.
+            //In this case our server socket is IPAddress_OF_THIS_COMPUTER:1234
+            //the IP_ADRESS in the socket part is basically the ip address of the computer this server.java code is running on
 
-            //and the server therefore needs to listen on that particular port(or electrical socket as we like to call them)
-            //and when the client sends a request for connection on that particular port, we have a connection
-            //and thats it, makes sense? yh
-            ServerSocket serverSocket = new ServerSocket(2000);
+            ServerSocket serverSocket = new ServerSocket(1234);
+
+            //Reason for using try/catch block in this case, is because this port that we may be using (in our case 1234), may be used by
+            //some other application.
+
+            //serverSocket.accept basically sits there forever waiting for some client (some computer), to connect to this computer at the port we are
+            //listening on, again in our case we are waiting for some application to start connecting on port 1234 of this computer.
+            Socket socketToConnectUsWithOurFirstClient = serverSocket.accept();
+            
+            //Once we've got our first client(or the first computer weve connected to, we can now start doing stuff
+
+            //Before we can start interacting with our client, we need to make sure we get our streams ready so that we can read and write data to the client
+            DataOutputStream writeToClient = new DataOutputStream(new BufferedOutputStream(socketToConnectUsWithOurFirstClient.getOutputStream()));
+            DataInputStream listenToClient = new DataInputStream(new BufferedInputStream(socketToConnectUsWithOurFirstClient.getInputStream()));
+           
+
+            //As you've already seen in the Client.java code, the Client starts sending a string input
+            //then sends an integer
+            //and then a boolean
+
+            //so lets starts taking these values in
+
+            String helloOutputFromClient = listenToClient.readUTF(); //take in the string input that the client first sends (which happens to be hello)
+            int integerOutputFromCLient = listenToClient.readInt(); //take in integer input from client, that happens to be 10, as programmed in the client.java code
+            Boolean booleanOutputFromClient = listenToClient.readBoolean(); //take in boolean input from client, in this case the boolean value
+            //the client outputted is "false"
+
+            //Lets print all these values out
+
+            System.out.println(helloOutputFromClient);
+            System.out.println(integerOutputFromCLient);
+            System.out.println(booleanOutputFromClient);
+
+            //Lets read the random input the client is now going to send us
+
+            String userClientOutput = listenToClient.readUTF();
+            //Now lets print it out
+            System.out.println(userClientOutput);
+
+            //Now lets try sending data to this client
+            if(userClientOutput.equals("hello")) {
+                writeToClient.writeUTF("Hello to you too");
+            } else {
+                writeToClient.writeUTF("Hello, there");
+            }
+            writeToClient.flush(); //As usual remember to flush after sending data.
+
+            //Finally close the streams and then the socket
+            writeToClient.close();
+            listenToClient.close();
+            socketToConnectUsWithOurFirstClient.close();
+
+            //Finally close the server socket
+            serverSocket.close();
+
+            //IMPORTANT: This server can only serve one client at a time (although it needs to be restarted every time it is ran with a new client)
+            //If 2 clients decide to connect together, or whilst one client is interacting with it, the server will not behave properly, and give
+            //undefined behavior, or more likely will just ignore the 2nd client.
+
         } catch (Exception e) {
             //TODO: handle exception
         }
